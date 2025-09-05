74207ca2748ae658694a640387135794ba080a0e
alvinfadli <alvinfdl21@gmail.com>
2025-09-06 01:37:52 +0700

    chore(test): revert domain change

--- apps/smtp-server/internal/server/server.go
+++ apps/smtp-server/internal/server/server.go
@@ -76,7 +76,7 @@ func Start(cfg *config.Config) {
 	s := smtp.NewServer(be)
 
 	s.Addr = cfg.Domain + ":" + cfg.Port
-	s.Domain = "fake-smtp.devhir.icu"
+	s.Domain = cfg.Domain
 	s.WriteTimeout = cfg.WriteTimeout
 	s.ReadTimeout = cfg.ReadTimeout
 	s.MaxMessageBytes = 1024 * 1024
